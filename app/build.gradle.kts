0c4bb17ff8e7e353003f54e9be084e783dfeafef
Huggo Parcelly <h.parcelly@gmail.com>
2025-02-19 22:54:49 -0300

    add fragment and adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.newsportal"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.newsportal"
---
+++ app/src/main/java/com/example/newsportal/NewsFragment.kt
@@ -0,0 +1,39 @@
+package com.example.newsportal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsFragment: Fragment() {
+
+    private lateinit var mRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mRecyclerView = view.findViewById(R.id.news_recycle_view)
+
+        mRecyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val newsList = NewsDatabase.findAll().shuffled()
+        val newsAadapter = NewsAdapter(newsList)
+        mRecyclerView.adapter = newsAadapter
+        newsAadapter.notifyDataSetChanged()
+    }
+}
